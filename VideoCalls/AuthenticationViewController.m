4733cd67b26241604ab3d91e56e2d723606c8af7
Julien Salort <mail@juliensalort.org>
2018-09-12 22:30:45 +0200

    Log error description on NC server notification subscription failure

--- VideoCalls/AuthenticationViewController.m
+++ VideoCalls/AuthenticationViewController.m
@@ -148,6 +148,7 @@ NSString * const kNCAuthTokenFlowEndpoint               = @"/index.php/login/flo
                 }];
             } else {
                 NSLog(@"Error while subscribing to NC server.");
+                NSLog([error localizedDescription]);
             }
         }];
         
