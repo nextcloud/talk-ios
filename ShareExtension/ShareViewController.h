82c7e6a3af9f7c9f71802d62f69cd6ffa627e8b5
Ivan Sein <ivan@nextcloud.com>
2020-07-16 16:37:54 +0200

    Add Share Extension.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

---
+++ ShareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  ShareExtension
+//
+//  Created by Ivan Sein on 16.07.20.
+//  Copyright © 2020 struktur AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
