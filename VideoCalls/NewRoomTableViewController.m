faacd4b51f9e9b0e1c902932e4a24b35d33c22e3
Ivan Sein <ivan@nextcloud.com>
2020-10-14 16:18:58 +0200

    Add license header.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- VideoCalls/NewRoomTableViewController.m
+++ VideoCalls/NewRoomTableViewController.m
@@ -1,10 +1,24 @@
-//
-//  NewRoomTableViewController.m
-//  VideoCalls
-//
-//  Created by Ivan Sein on 25.06.18.
-//  Copyright © 2018 struktur AG. All rights reserved.
-//
+/**
+ * @copyright Copyright (c) 2020 Ivan Sein <ivan@nextcloud.com>
+ *
+ * @author Ivan Sein <ivan@nextcloud.com>
+ *
+ * @license GNU GPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #import "NewRoomTableViewController.h"
 
