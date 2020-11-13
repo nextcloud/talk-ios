7d95f33935e0d391ea121edd345532bdf555caea
Marcel Müller <marcel-mueller@gmx.de>
2020-11-13 20:08:08 +0100

    Added BarButtonItemWithActivity
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

---
+++ NextcloudTalk/BarButtonItemWithActivity.h
@@ -0,0 +1,35 @@
+/**
+ * @copyright Copyright (c) 2020 Marcel Müller <marcel-mueller@gmx.de>
+ *
+ * @author Marcel Müller <marcel-mueller@gmx.de>
+ *
+ * @license GNU GPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface BarButtonItemWithActivity : UIBarButtonItem
+
+@property (nonatomic, strong) UIButton *innerButton;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+
+- (instancetype)initWithWidth:(CGFloat)buttonWidth withImage:(UIImage *)buttonImage;
+- (void)showActivityIndicator;
+- (void)hideActivityIndicator;
+
+@end
+
---
+++ NextcloudTalk/BarButtonItemWithActivity.m
@@ -0,0 +1,74 @@
+/**
+ * @copyright Copyright (c) 2020 Marcel Müller <marcel-mueller@gmx.de>
+ *
+ * @author Marcel Müller <marcel-mueller@gmx.de>
+ *
+ * @license GNU GPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#import "BarButtonItemWithActivity.h"
+#import "NCAppBranding.h"
+
+@interface BarButtonItemWithActivity ()
+
+@end
+
+@implementation BarButtonItemWithActivity
+
+- (instancetype)init {
+    return [super init];
+}
+
+- (instancetype)initWithWidth:(CGFloat)buttonWidth withImage:(UIImage *)buttonImage {
+    self = [self init];
+    
+    if (self) {
+        UIColor *themeColor = [NCAppBranding themeTextColor];
+        
+        // Use UIButton as CustomView in UIBarButtonItem to have a fixed-size item
+        self.innerButton = [[UIButton alloc] init];
+
+        [self.innerButton setImage:buttonImage forState:UIControlStateNormal];
+        self.innerButton.tintColor = UIColor.whiteColor;
+        self.innerButton.frame = CGRectMake(0, 0, buttonWidth, 0);
+        self.innerButton.tintColor = themeColor;
+        
+        // Make sure the size of UIBarButtonItem stays the same when displaying the ActivityIndicator
+        self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+        self.activityIndicator.tintColor = themeColor;
+        self.activityIndicator.frame = CGRectMake(0, 0, buttonWidth, 0);
+        
+        [self setCustomView:self.innerButton];
+    }
+    
+    return self;
+}
+
+- (void)showActivityIndicator
+{
+    [self.activityIndicator startAnimating];
+    [self setCustomView:self.activityIndicator];
+}
+
+- (void)hideActivityIndicator
+{
+    [self setCustomView:self.innerButton];
+    [self.activityIndicator stopAnimating];
+    
+}
+
+@end
