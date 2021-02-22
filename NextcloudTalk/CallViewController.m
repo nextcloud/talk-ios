9c3742f1ff799d1b7609fabaa3c1234cd8750334
Ivan Sein <ivan@nextcloud.com>
2021-02-22 16:06:28 +0100

    Do not allow localVideoView go under "chat toggle" and "close screensharing" buttons.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/CallViewController.m
+++ NextcloudTalk/CallViewController.m
@@ -737,7 +737,10 @@ typedef NS_ENUM(NSInteger, CallState) {
         position = CGPointMake(position.x, edgeInsets.top);
     }
     // Adjust right
-    if (position.x > parentSize.width - viewSize.width - edgeInsets.right) {
+    BOOL isChatButtonVisible = _toggleChatButton.frame.origin.x < parentSize.width;
+    if (isChatButtonVisible && position.x > _toggleChatButton.frame.origin.x - viewSize.height - edgeInsets.right) {
+        position = CGPointMake(_toggleChatButton.frame.origin.x - viewSize.width - edgeInsets.right, position.y);
+    } else if (position.x > parentSize.width - viewSize.width - edgeInsets.right) {
         position = CGPointMake(parentSize.width - viewSize.width - edgeInsets.right, position.y);
     }
     // Adjust bottom
