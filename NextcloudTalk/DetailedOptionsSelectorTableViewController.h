fa3c9190eb987cac5f7f3d1d65e3ba6f71bd7e55
Ivan Sein <ivan@nextcloud.com>
2021-03-31 14:45:08 +0200

    Show detailed description for scopes.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

---
+++ NextcloudTalk/DetailedOptionsSelectorTableViewController.h
@@ -0,0 +1,48 @@
+/**
+ * @copyright Copyright (c) 2021 Ivan Sein <ivan@nextcloud.com>
+ *
+ * @author Ivan Sein <ivan@nextcloud.com>
+ *
+ * @license GNU GPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface DetailedOption : NSObject
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+@property (nonatomic, copy) UIImage *image;
+@property (nonatomic, assign) BOOL selected;
+@end
+
+@class DetailedOptionsSelectorTableViewController;
+@protocol DetailedOptionsSelectorTableViewControllerDelegate <NSObject>
+- (void)detailedOptionsSelector:(DetailedOptionsSelectorTableViewController *)viewController didSelectOptionWithIdentifier:(DetailedOption *)option;
+@end
+
+@interface DetailedOptionsSelectorTableViewController : UITableViewController
+
+@property (nonatomic, weak) id<DetailedOptionsSelectorTableViewControllerDelegate> delegate;
+@property (nonatomic, strong) NSArray *options;
+@property (nonatomic, strong) NSString *senderId;
+@property (nonatomic, strong) NSString *title;
+
+- (instancetype)initWithOptions:(NSArray *)options forSenderIdentifier:(NSString *)senderId andTitle:(NSString *)title;
+
+@end
+
