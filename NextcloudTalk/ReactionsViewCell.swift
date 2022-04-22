4b473ac1db018d451573110f4e524e9fb0341118
Ivan Sein <ivan@nextcloud.com>
2022-04-22 20:15:14 +0200

    Make reaction chip width adjustable.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/ReactionsViewCell.swift
+++ NextcloudTalk/ReactionsViewCell.swift
@@ -49,10 +49,35 @@ import UIKit
         return UIColor.lightGray.cgColor
     }
 
+    func sizeForReaction(reaction: NCChatReaction) -> CGSize {
+        let text = textForReaction(reaction: reaction)
+        var size = CGSize(width: text.width(withConstrainedHeight: 30, font: .systemFont(ofSize: 13.0)), height: 30)
+        size.width += 20
+        return size
+    }
+
+    func textForReaction(reaction: NCChatReaction) -> String {
+        return reaction.reaction + " " + String(reaction.count)
+    }
+
     func setReaction(reaction: NCChatReaction) {
-        label.text = reaction.reaction + " " + String(reaction.count)
+        label.text = textForReaction(reaction: reaction)
         label.backgroundColor = reaction.userReacted ? NCAppBranding.elementColor().withAlphaComponent(0.15) : NCAppBranding.backgroundColor()
         label.layer.borderColor = reaction.userReacted ? NCAppBranding.elementColor().cgColor : self.borderColor()
     }
 
+}
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+        return ceil(boundingBox.width)
+    }
 }
