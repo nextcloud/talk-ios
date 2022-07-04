3a5ece2e6b03d82c39219e187007cd2e1ea5d2e1
Marcel Müller <marcel-mueller@gmx.de>
2022-07-04 17:31:01 +0200

    Fix swiftlint warnings
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/UserStatusMessageViewController.swift
+++ NextcloudTalk/UserStatusMessageViewController.swift
@@ -409,10 +409,8 @@ extension UserStatusMessageViewController: UITableViewDataSource {
     override var textInputContextIdentifier: String? { "" } // return non-nil to show the Emoji keyboard ¯\_(ツ)_/¯
 
     override var textInputMode: UITextInputMode? {
-        for mode in UITextInputMode.activeInputModes {
-            if mode.primaryLanguage == "emoji" {
-                return mode
-            }
+        for mode in UITextInputMode.activeInputModes where mode.primaryLanguage == "emoji" {
+            return mode
         }
         return nil
     }
