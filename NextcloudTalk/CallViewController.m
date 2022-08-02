99d3f203a8b44a208bc283f8d4b1204f69df2988
Ivan Sein <ivan@nextcloud.com>
2022-08-02 13:54:18 +0200

    Check current route outputs before accessing first element.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/CallViewController.m
+++ NextcloudTalk/CallViewController.m
@@ -804,7 +804,10 @@ typedef NS_ENUM(NSInteger, CallState) {
 - (void)adjustSpeakerButton
 {
     AVAudioSession *audioSession = [NCAudioController sharedInstance].rtcAudioSession.session;
-    AVAudioSessionPortDescription *currentOutput = audioSession.currentRoute.outputs[0];
+    AVAudioSessionPortDescription *currentOutput = nil;
+    if (audioSession.currentRoute.outputs.count > 0) {
+        currentOutput = audioSession.currentRoute.outputs[0];
+    }
     if ([currentOutput.portType isEqualToString:AVAudioSessionPortBuiltInSpeaker]) {
         [self setSpeakerButtonActive:YES showInfoToast:NO];
     } else {
