961aed9b90ba21dd5ecc92cded10755741a328a8
Marcel Müller <marcel-mueller@gmx.de>
2022-10-04 16:09:58 +0200

    Set preferredSearchBarPlacement when running on iOS 16
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/AddParticipantsTableViewController.m
+++ NextcloudTalk/AddParticipantsTableViewController.m
@@ -90,6 +90,11 @@
 
     self.navigationItem.searchController = _searchController;
     self.navigationItem.searchController.searchBar.searchTextField.backgroundColor = [NCUtils searchbarBGColorForColor:themeColor];
+
+    if (@available(iOS 16.0, *)) {
+        self.navigationItem.preferredSearchBarPlacement = UINavigationItemSearchBarPlacementStacked;
+    }
+    
     _searchController.searchBar.tintColor = [NCAppBranding themeTextColor];
     UITextField *searchTextField = [_searchController.searchBar valueForKey:@"searchField"];
     UIButton *clearButton = [searchTextField valueForKey:@"_clearButton"];
