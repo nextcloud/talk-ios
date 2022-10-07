0d9486d9edf59036ff76dc78d6f3827a35d40a08
Ivan Sein <ivan@nextcloud.com>
2022-10-07 18:33:50 +0200

    Store mentions as message paramenters in temporary message. This allows to send mentions correctly when resending a failed message and highlight mentions in temporary messages.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/NCMessageParameter.m
+++ NextcloudTalk/NCMessageParameter.m
@@ -48,6 +48,8 @@
         
         self.contactName = [parameterDict objectForKey:@"contact-name"];
         self.contactPhoto = [parameterDict objectForKey:@"contact-photo"];
+        self.mentionId = [parameterDict objectForKey:@"mentionId"];
+        self.mentionDisplayName = [parameterDict objectForKey:@"mentionDisplayName"];
     }
     
     return self;
@@ -71,6 +73,109 @@
     }
     
     return nil;
+}
+
++ (NSDictionary *)dictionaryFromMessageParameter:(NCMessageParameter *)messageParameter
+{
+    if (!messageParameter || ![messageParameter isKindOfClass:[NCMessageParameter class]]) {
+        return nil;
+    }
+
+    NSMutableDictionary *messageParameterDict = [NSMutableDictionary new];
+    if (messageParameter.parameterId) {
+        [messageParameterDict setObject:messageParameter.parameterId forKey:@"id"];
+    }
+    if (messageParameter.name) {
+        [messageParameterDict setObject:messageParameter.name forKey:@"name"];
+    }
+    if (messageParameter.link) {
+        [messageParameterDict setObject:messageParameter.link forKey:@"link"];
+    }
+    if (messageParameter.type) {
+        [messageParameterDict setObject:messageParameter.type forKey:@"type"];
+    }
+    if (messageParameter.contactName) {
+        [messageParameterDict setObject:messageParameter.contactName forKey:@"contact-name"];
+    }
+    if (messageParameter.contactPhoto) {
+        [messageParameterDict setObject:messageParameter.contactPhoto forKey:@"contact-photo"];
+    }
+    if (messageParameter.mentionId) {
+        [messageParameterDict setObject:messageParameter.mentionId forKey:@"mentionId"];
+    }
+    if (messageParameter.mentionDisplayName) {
+        [messageParameterDict setObject:messageParameter.mentionDisplayName forKey:@"mentionDisplayName"];
+    }
+
+    return [[NSDictionary alloc] initWithDictionary:messageParameterDict];
+}
+
+
++ (NSDictionary *)messageParametersDictionaryFromDictionary:(NSDictionary *)parametersDict
+{
+    if (!parametersDict || ![parametersDict isKindOfClass:[NSDictionary class]]) {
+        return nil;
+    }
+
+    NSMutableDictionary *messageParametersDict = [NSMutableDictionary new];
+    for (NSString *parameterKey in parametersDict) {
+        NCMessageParameter *parameter = [parametersDict objectForKey:parameterKey];
+        NSDictionary *parameterDict = [NCMessageParameter dictionaryFromMessageParameter:parameter];
+        if (parameterDict) {
+            [messageParametersDict setObject:parameterDict forKey:parameterKey];
+        }
+    }
+
+    return [[NSDictionary alloc] initWithDictionary:messageParametersDict];
+}
+
++ (NSString *)messageParametersJSONStringFromDictionary:(NSDictionary *)parametersDict
+{
+    if (!parametersDict || ![parametersDict isKindOfClass:[NSDictionary class]]) {
+        return nil;
+    }
+
+    NSString *messageParametersJSONString = nil;
+    NSDictionary *messageParameters = [self messageParametersDictionaryFromDictionary:parametersDict];
+    if ([messageParameters isKindOfClass:[NSDictionary class]]) {
+        NSError *error;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:messageParameters
+                                                           options:0
+                                                             error:&error];
+        if (jsonData) {
+            messageParametersJSONString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        } else {
+            NSLog(@"Error generating message parameters JSON string: %@", error);
+        }
+    }
+
+    return messageParametersJSONString;
+}
+
++ (NSDictionary *)messageParametersDictFromJSONString:(NSString *)parametersJSONString
+{
+    NSDictionary *parametersDict = @{};
+    NSData *data = [parametersJSONString dataUsingEncoding:NSUTF8StringEncoding];
+    if (data) {
+        NSError* error;
+        NSDictionary* jsonData = [NSJSONSerialization JSONObjectWithData:data
+                                                                 options:0
+                                                                   error:&error];
+        if (jsonData) {
+            parametersDict = jsonData;
+        } else {
+            NSLog(@"Error retrieving message parameters JSON data: %@", error);
+        }
+    }
+    NSMutableDictionary *messageParametersDict = [NSMutableDictionary new];
+    for (NSString *parameterKey in parametersDict) {
+        NCMessageParameter *parameter = [[NCMessageParameter alloc] initWithDictionary:[parametersDict objectForKey:parameterKey]];
+        if (parameter) {
+            [messageParametersDict setObject:parameter forKey:parameterKey];
+        }
+    }
+
+    return messageParametersDict;
 }
 
 @end
