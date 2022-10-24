b743cb8e7eae03caf36c9cb5dbd6d528474a35df
Marcel Müller <marcel-mueller@gmx.de>
2022-10-24 22:50:25 +0200

    Don't handle life-cycle events if the chat was stopped
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCChatViewController.m
+++ NextcloudTalk/NCChatViewController.m
@@ -540,6 +540,11 @@ NSString * const NCChatViewControllerTalkToUserNotification = @"NCChatViewContro
         return;
     }
 
+    // If we stopped the chat, we don't want to resume it here
+    if (_hasStopped) {
+        return;
+    }
+
     // Check if new messages were added while the app was inactive (eg. via background-refresh)
     NCChatMessage *lastMessage = [[self->_messages objectForKey:[self->_dateSections lastObject]] lastObject];
     
@@ -557,6 +562,11 @@ NSString * const NCChatViewControllerTalkToUserNotification = @"NCChatViewContro
 
 -(void)appWillResignActive:(NSNotification*)notification
 {
+    // If we stopped the chat, we don't want to change anything here
+    if (_hasStopped) {
+        return;
+    }
+
     _hasReceiveNewMessages = NO;
     _startReceivingMessagesAfterJoin = YES;
     [self removeUnreadMessagesSeparator];
