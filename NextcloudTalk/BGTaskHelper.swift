b8ade4d1928d20e237c147e9947d1b669d87d4ac
Marcel Müller <marcel-mueller@gmx.de>
2022-10-26 17:16:39 +0200

    Don't acces UI* classes in app extension
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/BGTaskHelper.swift
+++ NextcloudTalk/BGTaskHelper.swift
@@ -23,7 +23,9 @@ import Foundation
 
 @objcMembers class BGTaskHelper: NSObject {
 
+#if !APP_EXTENSION
     var backgroundTaskIdentifier: UIBackgroundTaskIdentifier = .invalid
+#endif
 
     public class func startBackgroundTask(withName taskName: String? = nil, expirationHandler handler: ((BGTaskHelper) -> Void)? = nil) -> BGTaskHelper {
         let taskHelper = BGTaskHelper()
@@ -34,17 +36,21 @@ import Foundation
             }
         }
 
+#if !APP_EXTENSION
         let backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask(withName: taskName, expirationHandler: expirationhandler)
         taskHelper.backgroundTaskIdentifier = backgroundTaskIdentifier
+#endif
 
         return taskHelper
     }
 
     public func stopBackgroundTask() {
+#if !APP_EXTENSION
         if backgroundTaskIdentifier != .invalid {
             UIApplication.shared.endBackgroundTask(backgroundTaskIdentifier)
             backgroundTaskIdentifier = .invalid
         }
+#endif
     }
 
 }
