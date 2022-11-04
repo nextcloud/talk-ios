52b7cbe79b66ac0f4544f37afc0b87ecb9b825fa
rakekniven <2069590+rakekniven@users.noreply.github.com>
2022-11-05 00:06:52 +0100

    l10n: Fixed grammar
    Reported at Transifex.
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>
    
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>

--- NextcloudTalk/CallsFromOldAccountViewController.swift
+++ NextcloudTalk/CallsFromOldAccountViewController.swift
@@ -49,8 +49,8 @@ class CallsFromOldAccountViewController: UIViewController {
         let warning1 = NSLocalizedString("Calls from an old account were received.", comment: "")
         let warning2 = NSLocalizedString("This usually indicates that this device was previously used for an account, which was not properly removed from the server.", comment: "")
         let warning3 = NSLocalizedString("To resolve this issue, use the web interface and go to 'Settings' -> 'Security'.", comment: "")
-        let warning4 = NSLocalizedString("Under 'Devices & sessions' check if there are duplicated entries for the same device.", comment: "")
-        let warning5 = NSLocalizedString("Remove old duplicate entries and leave just the most recent entries.", comment: "")
+        let warning4 = NSLocalizedString("Under 'Devices & sessions' check if there are duplicate entries for the same device.", comment: "")
+        let warning5 = NSLocalizedString("Remove old duplicate entries and leave only the most recent entries.", comment: "")
         let warning6 = NSLocalizedString("If you're using multiple servers, you need to check all of them.", comment: "")
 
         let warningTextComplete = warning1 + " " + warning2 + "\n\n" + warning3 + "\n\n" + warning4 + "\n\n" + warning5 + "\n\n" + warning6
