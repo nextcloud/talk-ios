72adb1ae6315d767eac9eb7b7721efa70d530cb9
Marcel Müller <marcel-mueller@gmx.de>
2023-01-05 12:21:17 +0100

    Set cornerRadius based on width. Don't adjust the fontSize of the title label
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCChatTitleView.m
+++ NextcloudTalk/NCChatTitleView.m
@@ -61,12 +61,10 @@
     [self addSubview:self.contentView];
     self.contentView.frame = self.bounds;
 
-    self.image.layer.cornerRadius = 15.0f;
+    self.image.layer.cornerRadius = self.image.bounds.size.width / 2;
     self.image.clipsToBounds = YES;
     self.image.backgroundColor = [NCAppBranding avatarPlaceholderColor];
 
-    self.title.titleLabel.adjustsFontSizeToFitWidth = YES;
-    self.title.titleLabel.minimumScaleFactor = 0.85;
     [self.title setTitleColor:[NCAppBranding themeTextColor] forState:UIControlStateNormal];
 
     [self.subtitle setTextColor:[[NCAppBranding themeTextColor] colorWithAlphaComponent:0.7]];
