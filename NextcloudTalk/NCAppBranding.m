5aac386342e2a6fb55bb9b5ce77d81e2d952c70d
Ivan Sein <ivan@nextcloud.com>
2023-02-10 15:54:15 +0100

    Change build number to 2 for beta2.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/NCAppBranding.m
+++ NextcloudTalk/NCAppBranding.m
@@ -45,7 +45,7 @@ BOOL const multiAccountEnabled = YES;
 BOOL const useAppsGroup = YES;
 BOOL const forceDomain = NO;
 NSString * const domain = nil;
-NSString * const appAlternateVersion = @"16.0.0 (beta 1)";
+NSString * const appAlternateVersion = @"16.0.0 (beta 2)";
 
 + (NSString *)getAppVersionString
 {
