0d1c48cb623888af7db80d6c3bb40b692fe87d7b
Ivan Sein <ivan@nextcloud.com>
2023-03-15 12:49:43 +0100

    Use room display name when user has non-custom avatar as callview placeholder.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/CallViewController.m
+++ NextcloudTalk/CallViewController.m
@@ -609,6 +609,7 @@ typedef void (^UpdateCallParticipantViewCellBlock)(CallParticipantViewCell *cell
 
     if (_room.type == kNCRoomTypeOneToOne) {
         __weak AvatarBackgroundImageView *weakBGView = self.avatarBackgroundImageView;
+        __weak NSString *weakRoomDisplayName = self.room.displayName;
         [self.avatarBackgroundImageView setImageWithURLRequest:[[NCAPIController sharedInstance] createAvatarRequestForUser:_room.name withStyle:self.traitCollection.userInterfaceStyle andSize:96 usingAccount:[[NCDatabaseManager sharedInstance] activeAccount]]
                                               placeholderImage:nil success:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, UIImage * _Nonnull image) {
                                                   NSDictionary *headers = [response allHeaderFields];
@@ -625,8 +626,8 @@ typedef void (^UpdateCallParticipantViewCellBlock)(CallParticipantViewCell *cell
                                                       [weakBGView setImage:blurImage];
                                                       weakBGView.contentMode = UIViewContentModeScaleAspectFill;
                                                   } else {
-                                                      DBImageColorPicker *colorPicker = [[DBImageColorPicker alloc] initFromImage:image withBackgroundType:DBImageColorPickerBackgroundTypeDefault];
-                                                      [weakBGView setBackgroundColor:colorPicker.backgroundColor];
+                                                      UIColor *bgColor = [[ColorGenerator shared] usernameToColor:weakRoomDisplayName];
+                                                      [weakBGView setBackgroundColor:bgColor];
                                                       weakBGView.backgroundColor = [weakBGView.backgroundColor colorWithAlphaComponent:0.8];
                                                   }
                                               } failure:nil];
