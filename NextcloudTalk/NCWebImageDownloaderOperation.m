e672b9386b74ef880ce7b0d5d46bf78c895e8c50
Marcel Müller <marcel-mueller@gmx.de>
2023-05-03 12:31:47 +0200

    Allow self-trusted certificates in SDWebImage
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

---
+++ NextcloudTalk/NCWebImageDownloaderOperation.m
@@ -0,0 +1,37 @@
+/**
+ * @copyright Copyright (c) 2023 Marcel Müller <marcel-mueller@gmx.de>
+ *
+ * @author Marcel Müller <marcel-mueller@gmx.de>
+ *
+ * @license GNU GPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#import "CCCertificate.h"
+#import "NCWebImageDownloaderOperation.h"
+
+@implementation NCWebImageDownloaderOperation
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential * _Nullable))completionHandler
+{
+    if ([[CCCertificate sharedManager] checkTrustedChallenge:challenge]) {
+        completionHandler(NSURLSessionAuthChallengeUseCredential, [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust]);
+    } else {
+        completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+    }
+}
+
+@end
