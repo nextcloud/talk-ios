5ec99b22f32fade3a96d6cb21df6075ba6fab2cf
Marcel Müller <marcel-mueller@gmx.de>
2023-06-08 22:59:02 +0200

    Adjust build number to 5 for RC3
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCAppBranding.m
+++ NextcloudTalk/NCAppBranding.m
@@ -45,7 +45,7 @@ BOOL const multiAccountEnabled = YES;
 BOOL const useAppsGroup = YES;
 BOOL const forceDomain = NO;
 NSString * const domain = nil;
-NSString * const appAlternateVersion = @"17.0.0 RC 2";
+NSString * const appAlternateVersion = @"17.0.0 RC 3";
 
 + (NSString *)getAppVersionString
 {
