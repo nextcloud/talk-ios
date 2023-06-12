ebdbfffd9e80349d39dbaa2256117c5e0d82f046
Marcel Müller <marcel-mueller@gmx.de>
2023-06-12 15:22:59 +0200

    Adjust build number to 6 for 17.0.0 Release
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCAppBranding.m
+++ NextcloudTalk/NCAppBranding.m
@@ -45,7 +45,7 @@ BOOL const multiAccountEnabled = YES;
 BOOL const useAppsGroup = YES;
 BOOL const forceDomain = NO;
 NSString * const domain = nil;
-NSString * const appAlternateVersion = @"17.0.0 RC 3";
+NSString * const appAlternateVersion = @"";
 
 + (NSString *)getAppVersionString
 {
