7ed04db4fc5e19df99f5d87cd45ad4947e8669e4
Marcel Müller <marcel-mueller@gmx.de>
2023-10-26 17:31:03 +0200

    Show localizable difference and remove UTF-8 BOM
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- generate-localizable-strings-file.sh
+++ generate-localizable-strings-file.sh
@@ -5,7 +5,7 @@
 echo 'Generating Localizable.strings file...'
 cd NextcloudTalk
 genstrings -o en.lproj *.m *.swift ../ShareExtension/*.m ../NotificationServiceExtension/*.m ../ThirdParty/SlackTextViewController/Source/*.m
-iconv -f UTF-16LE -t UTF-8 en.lproj/Localizable.strings > en.lproj/Localizable-utf8.strings
+iconv -f UTF-16 -t UTF-8 en.lproj/Localizable.strings > en.lproj/Localizable-utf8.strings
 mv en.lproj/Localizable-utf8.strings en.lproj/Localizable.strings
 echo 'Localizable.strings file generated!'
 
