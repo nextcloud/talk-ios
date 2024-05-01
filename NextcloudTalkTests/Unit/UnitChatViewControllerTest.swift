e395755f7c7211fa160bd99bf22381c32dac9cae
Marcel Müller <marcel-mueller@gmx.de>
2024-05-02 00:15:51 +0200

    Add test for message expiration
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

---
+++ NextcloudTalkTests/Unit/UnitChatViewControllerTest.swift
@@ -0,0 +1,87 @@
+//
+// Copyright (c) 2024 Marcel Müller <marcel-mueller@gmx.de>
+//
+// Author Marcel Müller <marcel-mueller@gmx.de>
+//
+// GNU GPL version 3 or any later version
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import XCTest
+@testable import NextcloudTalk
+
+final class UnitChatViewControllerTest: TestBaseRealm {
+
+    func testLocalMention() throws {
+        let activeAccount = NCDatabaseManager.sharedInstance().activeAccount()
+        let roomName = "Expire Messages Test Room"
+        let roomToken = "expToken"
+
+        let room = NCRoom()
+        room.token = roomToken
+        room.name = roomName
+        room.accountId = activeAccount.accountId
+
+        // Create 2 messages which are in different sections
+        let expMessage1 = NCChatMessage()
+
+        expMessage1.internalId = "internal1"
+        expMessage1.accountId = activeAccount.accountId
+        expMessage1.actorDisplayName = activeAccount.userDisplayName
+        expMessage1.actorId = activeAccount.userId
+        expMessage1.actorType = "users"
+        expMessage1.timestamp = Int(Date().timeIntervalSince1970) - 1000000
+        expMessage1.expirationTimestamp = Int(Date().timeIntervalSince1970) - 1000
+        expMessage1.token = roomToken
+        expMessage1.message = "Message 1"
+
+        let expMessage2 = NCChatMessage()
+
+        expMessage2.internalId = "internal2"
+        expMessage2.accountId = activeAccount.accountId
+        expMessage2.actorDisplayName = activeAccount.userDisplayName
+        expMessage2.actorId = activeAccount.userId
+        expMessage2.actorType = "users"
+        expMessage2.timestamp = Int(Date().timeIntervalSince1970)
+        expMessage2.expirationTimestamp = Int(Date().timeIntervalSince1970) - 1000
+        expMessage2.token = roomToken
+        expMessage2.message = "Message 2"
+
+        try? realm.transaction {
+            realm.add(room)
+            realm.add(expMessage1)
+            realm.add(expMessage2)
+        }
+
+        XCTAssertEqual(NCChatMessage.allObjects().count, 2)
+
+        let chatViewController = ChatViewController(for: room)!
+        let messageArray = [expMessage1, expMessage2].map { NCChatMessage(value: $0) }
+
+        chatViewController.appendMessages(messages: messageArray)
+        chatViewController.removeExpiredMessages()
+
+        // Since removeExpiredMessages is dispatched, we need to wait until it was scheduled
+        var exp = expectation(description: "\(#function)\(#line)")
+
+        DispatchQueue.main.async {
+            exp.fulfill()
+        }
+
+        waitForExpectations(timeout: TestConstants.timeoutShort, handler: nil)
+
+        XCTAssertEqual(NCChatMessage.allObjects().count, 0)
+    }
+}
