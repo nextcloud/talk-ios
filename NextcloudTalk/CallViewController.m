a6c70ba0c8ed29f487611ccbdcc0424a26982dc2
Marcel Müller <marcel-mueller@gmx.de>
2024-07-08 10:27:08 +0200

    Close context menus before hanging up
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/CallViewController.m
+++ NextcloudTalk/CallViewController.m
@@ -1494,7 +1494,11 @@ typedef void (^UpdateCallParticipantViewCellBlock)(CallParticipantViewCell *cell
             [_chatViewController leaveChat];
             _chatViewController = nil;
         }
-        
+
+        // Make sure there's no menu interfering with our dismissal
+        [self.moreMenuButton.contextMenuInteraction dismissMenu];
+        [self.hangUpButton.contextMenuInteraction dismissMenu];
+
         [self.delegate callViewControllerWantsToBeDismissed:self];
         
         [_callController stopCapturing];
