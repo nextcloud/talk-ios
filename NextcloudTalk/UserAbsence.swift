0e8fd304bef30edfc1ca92ffcfa81aa8c7a1bcb4
Ivan Sein <ivan@nextcloud.com>
2024-12-02 11:59:58 +0100

    fix(API): Get correct values from Out-Of-Office API.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/UserAbsence.swift
+++ NextcloudTalk/UserAbsence.swift
@@ -8,11 +8,11 @@ import Foundation
 @objcMembers public class UserAbsence: NSObject {
 
     // See: https://docs.nextcloud.com/server/latest/developer_manual/client_apis/OCS/ocs-out-of-office-api.html
-    public var id: Int = 0
+    public var id: String?
     public var userId: String?
-    public var firstDay: String?
-    public var lastDay: String?
-    public var status: String?
+    public var startDate: Int?
+    public var endDate: Int?
+    public var shortMessage: String?
     public var message: String?
     public var replacementUserId: String?
     public var replacementUserDisplayName: String?
@@ -20,11 +20,11 @@ import Foundation
     init(dictionary: [String: Any]) {
         super.init()
 
-        self.id = dictionary["id"] as? Int ?? 0
+        self.id = dictionary["id"] as? String
         self.userId = dictionary["userId"] as? String
-        self.firstDay = dictionary["firstDay"] as? String
-        self.lastDay = dictionary["lastDay"] as? String
-        self.status = dictionary["status"] as? String
+        self.startDate = dictionary["startDate"] as? Int
+        self.endDate = dictionary["endDate"] as? Int
+        self.shortMessage = dictionary["status"] as? String
         self.message = dictionary["message"] as? String
         self.replacementUserId = dictionary["replacementUserId"] as? String
         self.replacementUserDisplayName = dictionary["replacementUserDisplayName"] as? String
