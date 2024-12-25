9bce713b58f48f1e7108e3a2f10172380bf76225
Marcel Müller <marcel-mueller@gmx.de>
2024-12-25 23:04:59 +0100

    fix(tests): Improve login reliability
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalkTests/UI/AAAALoginTest.swift
+++ NextcloudTalkTests/UI/AAAALoginTest.swift
@@ -15,18 +15,7 @@ final class AAAALoginTest: XCTestCase {
 
     // Tests are done in alphabetical order, so we want to always test login first
     func test_Login() {
-        let app = launchAndLogin()
-
-        // Check if the profile button is available
-        let profileButton = app.buttons["LoadedProfileButton"]
-        XCTAssert(profileButton.waitForExistence(timeout: TestConstants.timeoutLong))
-
-        // Open profile menu
-        profileButton.tap()
-
-        // At this point we should be logged in, so check if username and server is displayed somewhere
-        XCTAssert(app.buttons["Settings"].waitForExistence(timeout: TestConstants.timeoutShort))
-        XCTAssert(app.buttons["Add account"].waitForExistence(timeout: TestConstants.timeoutShort))
+        launchAndLogin()
     }
 
 }
