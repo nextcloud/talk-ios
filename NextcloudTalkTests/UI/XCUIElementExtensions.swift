82c02018ff3b29f0e28f2b1b8d1cab23947cbdac
Marcel Müller <marcel-mueller@gmx.de>
2025-03-26 16:41:39 +0100

    tests: Add test for room deletion
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

---
+++ NextcloudTalkTests/UI/XCUIElementExtensions.swift
@@ -0,0 +1,30 @@
+//
+// SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+
+import Foundation
+import XCTest
+
+extension XCUIElement {
+
+    // See: https://stackoverflow.com/a/63089781
+    /**
+     * Waits the specified amount of time for the element’s `exists` property to become `false`.
+     *
+     * - Parameter timeout: The amount of time to wait.
+     * - Returns: `false` if the timeout expires without the element coming out of existence.
+     */
+    func waitForNonExistence(timeout: TimeInterval) -> Bool {
+
+        let timeStart = Date().timeIntervalSince1970
+
+        while (Date().timeIntervalSince1970 <= (timeStart + timeout)) {
+            if !exists { return true }
+
+            usleep(500)
+        }
+
+        return false
+    }
+}
