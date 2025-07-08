7a9fe1d89a8c248fec3f1adfeac7ca1e82409710
Ivan Sein <ivan@nextcloud.com>
2025-07-08 10:00:45 +0200

    chore(login): Update copyright comments
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/QRScannerViewController.swift
+++ NextcloudTalk/QRScannerViewController.swift
@@ -1,7 +1,7 @@
-/**
- * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+//
+// SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
 
 import UIKit
 import VisionKit
