da4f2aa361c8acf9b7564085d7ce0b5848faa764
Ivan Sein <ivan@nextcloud.com>
2025-07-29 17:02:18 +0200

    feat(threads): Adjust NCChatController to support threads.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/NCChatBlock.h
+++ NextcloudTalk/NCChatBlock.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) NSString *internalId; // accountId@token (same as room internal id)
 @property (nonatomic, strong) NSString *accountId;
 @property (nonatomic, strong) NSString *token;
+@property (nonatomic, assign) NSInteger threadId;
 @property (nonatomic, assign) NSInteger oldestMessageId;
 @property (nonatomic, assign) NSInteger newestMessageId;
 @property (nonatomic, assign) BOOL hasHistory;
