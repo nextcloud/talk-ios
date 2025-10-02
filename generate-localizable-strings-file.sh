18ff196005235b53d4a836b306e7f96a87cf87d0
Marcel Müller <marcel-mueller@gmx.de>
2025-10-02 11:06:43 +0200

    fix: Translations after build folders
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- generate-localizable-strings-file.sh
+++ generate-localizable-strings-file.sh
@@ -7,7 +7,7 @@
 
 echo 'Generating Localizable.strings file...'
 cd NextcloudTalk
-genstrings -o en.lproj -SwiftUI *.m *.swift ../ShareExtension/*.m ../ShareExtension/*.swift ../NotificationServiceExtension/*.m ../BroadcastUploadExtension/*.swift ../TalkIntents/*.swift ../ThirdParty/SlackTextViewController/Source/*.m
+find ../ -name "*.swift" -print0 -or -name "*.m" -not -path "../Pods/*" -print0 | xargs -0 genstrings -o en.lproj -SwiftUI
 iconv -f UTF-16 -t UTF-8 en.lproj/Localizable.strings > en.lproj/Localizable-utf8.strings
 mv en.lproj/Localizable-utf8.strings en.lproj/Localizable.strings
 echo 'Localizable.strings file generated!'
