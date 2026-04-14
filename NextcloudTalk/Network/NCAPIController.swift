58c3d14135c183cd657560591140ee107095a71f
Marcel Müller <marcel-mueller@gmx.de>
2026-04-14 22:37:11 +0200

    fix: JSON encoding for talkMetaData
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/Network/NCAPIController.swift
+++ NextcloudTalk/Network/NCAPIController.swift
@@ -2985,7 +2985,7 @@ class NCAPIController: NSObject, NKCommonDelegate {
         }
 
         if let talkMetaData, let jsonData = try? JSONSerialization.data(withJSONObject: talkMetaData) {
-            parameters["talkMetaData"] = jsonData
+            parameters["talkMetaData"] = String(data: jsonData, encoding: .utf8)!
         }
 
         apiSessionManager.postOcs(urlString, account: account, parameters: parameters) { _, ocsError in
