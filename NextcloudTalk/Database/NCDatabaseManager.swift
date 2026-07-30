fe0f1bfd7354cffc8586e76215bd9576f96c439d
Marcel Müller <marcel-mueller@gmx.de>
2026-07-30 19:04:16 +0200

    fix: Bump database schema
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/Database/NCDatabaseManager.swift
+++ NextcloudTalk/Database/NCDatabaseManager.swift
@@ -7,7 +7,7 @@ import Foundation
 
 public let kTalkDatabaseFolder = "Library/Application Support/Talk"
 public let kTalkDatabaseFileName = "talk.realm"
-public let kTalkDatabaseSchemaVersion: UInt64 = 92
+public let kTalkDatabaseSchemaVersion: UInt64 = 93
 
 // Objective-C bridge for the Talk database constants that are still referenced from Objective-C code.
 // These reference the Swift values and can be removed once those call sites are migrated to Swift.
